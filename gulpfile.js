6bac9fab9b36744370b26e10d66afaf236cd9c70
unknown <xiaoxm_001@163.com>
2016-07-04 06:47:05 +0800

    first

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp =require('gulp');
+const browserSync =require('browser-sync').create();
+
+gulp.task('serve',function () {
+    browserSync.init({
+        server:{
+            baseDir:"app"
+        },
+        port:177
+    });
+})
+
+
+
+
+
